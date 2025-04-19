7b328fad20e4713fcb2cdc61f681eb6219314d04
Josh Rime <joshrime1990@icloud.com>
2025-04-19 02:53:38 -0500

    Schema built in model, revamp port

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { connectDB } from './db/connectDB.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
 
-dotenv.config()
-const app = express()
-
-
-app.get('/', (req,res) => {
-    res.send('Hello World from the backend!')
-})
-
-
-app.use('/api/auth', authRoutes)
-
-
-app.listen(3000, () => {
-    connectDB()
-    console.log('\n---Server is running on http://localhost:3000---\n');
-})
-
-
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`\n=== Server is running on Port: ${PORT} ===\n`);
+});
