caa4c4131ec37ede6f2a85a4a1bd16152136f120
Josh Rime <joshrime1990@icloud.com>
2025-04-19 03:27:04 -0500

    setup up hash, vertoken, new user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,12 +1,43 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const signup = async (req,res) => {
-    res.send('signup working route')
-}
-export const login = async (req,res) => {
-    res.send('login working route')
-}
-export const logout = async (req,res) => {
-    res.send('logout working route')
-}
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
 
+  try {
+    if (!name || !email || !password) {
+      throw new Error("All fields are required");
+    }
 
+    const userAlreadyExists = await User.findOne({ email });
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exist",
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+
+    const user = await User.create({
+      password: hashedPassword,
+      email,
+      name,
+      verificationToken,
+      verificationExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const login = async (req, res) => {
+  res.send("login working route");
+};
+export const logout = async (req, res) => {
+  res.send("logout working route");
+};
