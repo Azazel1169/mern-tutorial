454e2d3611270eeda2c65fc24c695858c7aec7e1
Josh Rime <joshrime1990@icloud.com>
2025-04-20 01:51:42 -0500

    verification email setup

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -2,30 +2,12 @@ import { MailtrapClient } from "mailtrap";
 import dotenv from "dotenv";
 dotenv.config();
 
-const TOKEN = process.env.MAILTRAP_TOKEN;
-const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
-
-const client = new MailtrapClient({
-  endpoint: ENDPOINT,
-  token: TOKEN,
+export const mailtrapClient = new MailtrapClient({
+  endpoint: process.env.MAILTRAP_ENDPOINT,
+  token: process.env.MAILTRAP_TOKEN,
 });
 
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.co",
   name: "Mailtrap Test",
 };
-const recipients = [
-  {
-    email: "joshrime1990@icloud.com",
-  },
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
